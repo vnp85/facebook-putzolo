a552d17ec11f2d5c377ce2f38789c38c9381363c
vnp85 <palnagy@gmail.com>
2016-12-14 13:14:18 +0200

    more elegant cheesy keywords handling
    Besides the pairs of keywords-exceptions, the code is restructred. The
    timer functions´ toString is overwritten.

--- facebook-putzolo.js
+++ facebook-putzolo.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         facebook putzolo
 // @namespace    http://csillagtura.ro/less-facebook-suggestions-userscript
-// @version      2016.11.29. 08:31
-// @description  hides facebook dom elements like the annoying suggested posts/pages/people
+// @version      2016.12.14. 10:55
+// @description  hides facebook dom elements like the annoying suggested posts/pages/people, needs fb to be in english
 // @author       VNP
 // @match        https://www.facebook.com/*
 // @run-at       document-start
@@ -12,107 +12,15 @@
 (function() {
     'use strict';
 
-    function putzolo_eventFire(el, etype){
-      if (el.fireEvent) {
-        el.fireEvent('on' + etype);
-      } else {
-        var evObj = document.createEvent('Events');
-        evObj.initEvent(etype, true, false);
-        el.dispatchEvent(evObj);
-      }
-    }
-    function putzolo_fewPostVisibleSoClickMoreButton(){
-        var contCol = document.getElementById("contentCol");
-        if (!contCol){
-            contCol = document.getElementById("contentArea");
-        }
-        if (!contCol){
-            return ;
-        }
-        if (contCol.clientHeight < 1200){
-            var lista = document.getElementsByTagName("a");
-            for (var q=0; q<lista.length; q++){
-                 if (lista[q].getAttribute("role") == "button"){
-                   if (lista[q].innerHTML.toLowerCase().indexOf("more stories") > -1){
-                       putzolo_eventFire(lista[q], "click");
-                   }
-                 };
-            }
-        } 
-    }
-    function putzolo_stringFitsCheesyKeywords(s){
-        var hide = new Array(
-            "kar"+String.fromCharCode(225)+"csony", 
-            "h"+String.fromCharCode(250)+
-            "sv"+String.fromCharCode(233)+"t", 
-            'christmas', 
-            '"+String.fromCharCode(252)+"nnepek', 
-            "happy new year", 
-            "isten "+String.fromCharCode(233)+"ltessen", 
-            "isten eltessen", 
-            "boldog szuletesnapot", 
-            "boldog sz"+String.fromCharCode(252)+"let"+String.fromCharCode(233)+"snapot", 
-            "boldog szulinapot", 
-            "boldog sz"+String.fromCharCode(252)+"linapot"
-        );
-        var but_keep = new Array(
-            "kar"+String.fromCharCode(225)+"csonyi zsolt", 
-            "kar"+String.fromCharCode(225)+"csony ben"
-        );
-        
-        s = s.toLowerCase();
-        for (var q=0; q<hide.length; q++){
-            if (s.indexOf(hide[q]) > -1){
-                var b = true;
-                for (var i = 0; i<but_keep.length; i++){
-                     if (s.indexOf(but_keep[q]) > -1){
-                         b = false;
-                     }
-                }
-                if (b){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }    
-    function putzolo_postFitsCheesyKeywords(d){
-        var at = "data-kwchk";
-        var a = d.getAttribute(at);
-        if (a){
-            return (a=="3");
-        }else{
-            var s = d.innerHTML.split("<"+"form")[0];
-            if (putzolo_stringFitsCheesyKeywords(s)){
-                d.setAttribute(at, "3");
-                return true;
-            }else{
-                d.setAttribute(at, "2");
-                return false;
-            }
-        }
-    }
-    function putzolo_isToBeHiddenJudgingByText(s){
-         if (
-                     (s.indexOf("invite friends to like") > -1) ||
-                     (s.indexOf("sponsored") > -1) ||
-                     (s.indexOf("suggested pages") > -1) ||
-                     (s.indexOf("buy and sell groups") > -1) ||
-                     (s.indexOf("sale groups") > -1) ||
-                     (s.indexOf("suggested groups") > -1) ||
-                     (s.indexOf("page posts you") > -1) ||
-                     (s.indexOf("you may like") > -1) ||
-                     (s.indexOf("by writing a review") > -1) ||
-                     (s.indexOf("people you may know") > -1) ||
-                     (s.indexOf("improve your news feed") > -1) ||
-                     (s.indexOf("suggested people") > -1)
-        ){ 
-             return true;
-        };        
-        return false;
-    }
-    
-    // taking over the timeouts and intervals
+    /* 
+
+    TIME
+
+    taking over the timeouts and intervals,
+    preventing loss of control
+
+    */
+
     window.putzoloInterval = -1;
     window.kliorInterval = window.clearInterval;
     window.kliorTimeout = window.clearTimeout;
@@ -124,14 +32,67 @@
                window.kliorInterval(intervalId); 
            }
         };
+        window.clearInterval.toString = function (){
+            return "function clearInterval() { [native code] }"
+        }
         window.clearTimeout = function(intervalId) {
            if (intervalId != window.putzoloInterval){
                window.kliorTimeout(intervalId); 
            }
         };
+        window.clearTimeout.toString = function (){
+            return "function clearTimeout() { [native code] }"
+        }
     }, 10);
-    
     console.log("putzolo inited, intervalhandlers grabbed");
+
+    
+    
+    /*
+
+    SIZE    
+
+    the feed might get short - single post - after
+    hiding feed elements fitting the criteria.
+    execute a click on the more button
+
+    */
+    function putzolo_eventFire(el, etype){
+      if (el.fireEvent) {
+        el.fireEvent('on' + etype);
+      } else {
+        var evObj = document.createEvent('Events');
+        evObj.initEvent(etype, true, false);
+        el.dispatchEvent(evObj);
+      }
+    }
+    function putzolo_fewPostsVisibleSoClickTheMoreButton(){
+        var contCol = document.getElementById("contentCol");
+        if (!contCol){
+            contCol = document.getElementById("contentArea");
+        }
+        if (!contCol){
+            return ;
+        }
+        if (contCol.clientHeight < 2000){
+            var lista = document.getElementsByTagName("a");
+            for (var q=0; q<lista.length; q++){
+                 if (lista[q].getAttribute("role") == "button"){
+                   if (lista[q].innerHTML.toLowerCase().indexOf("more stories") > -1){
+                       putzolo_eventFire(lista[q], "click");
+                   }
+                 };
+            }
+        } 
+    }
+
+    /*
+    
+    UI
+    
+    Include some changes to the UI marking that putzolo has control
+    
+    */
     
     function putzolo_putUIElements(){        
         var head = document.head || document.getElementsByTagName('head')[0];
@@ -198,7 +159,109 @@
     };
     
     putzolo_putUIElements();
+
+    /*
+    CHEESY
     
+    Cats can not be hidden yet, but strings/posts that fit a keyword, can
+    
+    */
+    
+    function putzolo_getTheCheesyPairs(){
+        var pairs = [];
+        pairs.push({
+             killIf:    ["kar"+String.fromCharCode(225)+"csony", "karacsony"],
+             butKeepIf: ["kar"+String.fromCharCode(225)+"csonyi zsolt", "kar"+String.fromCharCode(225)+"csony ben", "zsolt kar", "karacsony beno", "karacsonyi zsolt" ]
+        })
+        pairs.push({
+             killIf:    ["christmas"],
+             butKeepIf: ["last christmas"]
+        })
+        pairs.push({
+           killIf: [
+            String.fromCharCode(252)+"nnepek", 
+            "husvet", 
+            "h"+String.fromCharCode(250)+"sv"+String.fromCharCode(233)+"t", 
+            "happy new year",
+            "isten "+String.fromCharCode(233)+"ltessen", 
+            "isten eltessen", 
+            "boldog szuletesnapot", 
+            "boldog sz"+String.fromCharCode(252)+"let"+String.fromCharCode(233)+"snapot", 
+            "boldog szulinapot", 
+            "boldog sz"+String.fromCharCode(252)+"linapot"
+                  ]
+        });
+        return pairs;
+    }
+    function putzolo_stringFitsCheesyKeywords(s){
+        var p = putzolo_getTheCheesyPairs();        
+        s = s.toLowerCase();
+        
+        for (var q=0; q<p.length; q++){
+            var kill = false;
+            for (var i=0; i<p[q].killIf.length; i++){
+                if (s.indexOf(p[q].killIf[i]) > -1){
+                    kill = true;
+                }
+            }
+            if (p[q].butKeepIf){
+                for (var i=0; i<p[q].butKeepIf.length; i++){
+                    if (s.indexOf(p[q].butKeepIf[i]) > -1){
+                        kill = false;
+                    }
+                }
+            }
+            if (kill){
+                return true;
+            }
+        }
+        
+        return false;
+    }    
+    function putzolo_postFitsCheesyKeywords(d){
+        var at = "data-kwchk";
+        var a = d.getAttribute(at);
+        if (a){
+            return (a=="3");
+        }else{
+            var s = d.innerHTML.split("<"+"form")[0];
+            if (putzolo_stringFitsCheesyKeywords(s)){
+                d.setAttribute(at, "3");
+                return true;
+            }else{
+                d.setAttribute(at, "2");
+                return false;
+            }
+        }
+    }
+    
+    /*
+    
+    PUTZOLO
+    
+    Clean up the UI from suggestions etc
+    
+    */
+    
+    function putzolo_isToBeHiddenJudgingByText(s){
+         if (
+                     (s.indexOf("invite friends to like") > -1) ||
+                     (s.indexOf("sponsored") > -1) ||
+                     (s.indexOf("suggested pages") > -1) ||
+                     (s.indexOf("buy and sell groups") > -1) ||
+                     (s.indexOf("sale groups") > -1) ||
+                     (s.indexOf("suggested groups") > -1) ||
+                     (s.indexOf("page posts you") > -1) ||
+                     (s.indexOf("you may like") > -1) ||
+                     (s.indexOf("by writing a review") > -1) ||
+                     (s.indexOf("people you may know") > -1) ||
+                     (s.indexOf("improve your news feed") > -1) ||
+                     (s.indexOf("suggested people") > -1)
+        ){ 
+             return true;
+        };        
+        return false;
+    }
     //setting up the interval that cleans the screen   
     window.putzoloInterval = setInterval(function (){
         var hidden_in_this_round = 0; 
@@ -280,6 +343,7 @@
             }
         }
         
+        // show feedback on the UI
         if (hidden_in_this_round > 0){
             if (window.putzolo_counter){
                 window.putzolo_counter.innerHTML = parseInt(window.putzolo_counter.innerHTML) + hidden_in_this_round;
@@ -293,7 +357,7 @@
                 window.putzolo_ticker.style.backgroundColor = "white";
             }
         }
-        putzolo_fewPostVisibleSoClickMoreButton();
+        putzolo_fewPostsVisibleSoClickTheMoreButton();
     }, 1000);
     
 })();
